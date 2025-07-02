04e15ef8e2b3dae0942b8a5ebfcd2496da18834b
MinatoCodes <subinsirmal11@gmail.com>
2025-07-02 21:45:22 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const login = require("fca-unofficial");
+const fs = require("fs");
+const downloader = require("./scripts/events/downloader");
+
+login({ appState: JSON.parse(fs.readFileSync("appstate.json", "utf8")) }, (err, api) => {
+  if (err) return console.error(err);
+
+  api.setOptions({ listenEvents: true });
+
+  api.listenMqtt((err, event) => {
+    if (err) return console.error(err);
+    if (event.type === "message" || event.type === "message_reply") {
+      downloader.onEvent({ api, event });
+    }
+  });
+});
+      
